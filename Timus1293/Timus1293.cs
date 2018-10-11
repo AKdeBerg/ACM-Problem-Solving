19e86836e7321223cea71c3af7cf50eec924bcf8
AKdeBerg <ich.bin.anik@gmail.com>
2018-10-11 11:40:53 +0600

    Add timus 1264 1293 1409

---
+++ Timus1293/Timus1293.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Timus1293
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int totalAmount = ((Int32.Parse(input[1]) * Int32.Parse(input[2])) * Int32.Parse(input[0])) * 2;
+            Console.WriteLine(totalAmount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Timus1409/Timus1409.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Timus1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int totalCan = ((int.Parse(input[0]) - 1)
+                 +(int.Parse(input[1]) - 1)
+                ) + 1;
+
+            Console.WriteLine("{0} {1}",totalCan - int.Parse(input[0]), totalCan - int.Parse(input[1]));
+            Console.ReadKey();
+        }
+    }
+}
