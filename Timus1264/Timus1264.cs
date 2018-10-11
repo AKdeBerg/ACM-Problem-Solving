b5c487f3b7dce17f0b84270cc32caba96e688004
AKdeBerg <ich.bin.anik@gmail.com>
2018-10-11 11:49:24 +0600

    Adding again the Timus1264

---
+++ Timus1264/Timus1264.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Timus1264
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            //long totalSeconds = Int64.Parse(input[0]) * (Int64.Parse(input[1]) + 1);
+
+            Console.WriteLine(Int64.Parse(input[0]) * (Int64.Parse(input[1]) + 1));
+            Console.ReadKey();
+        }
+    }
+}
